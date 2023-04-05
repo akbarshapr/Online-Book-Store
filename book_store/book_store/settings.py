8b241ee82c40396e3c6f7f022178d9a6bd2b91ab
Akbar Sha <akbarshapr@gmail.com>
2023-04-05 12:37:26 +0530

    Initial Commit

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -75,17 +75,24 @@ WSGI_APPLICATION = 'book_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': "bookstore",
+#         'USER': 'root',
+#         'PASSWORD': 'akbar@sha',
+#         'HOST': 'localhost',
+#         'PORT': 3306
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "bookstore",
-        'USER': 'root',
-        'PASSWORD': 'akbar@sha',
-        'HOST': 'localhost',
-        'PORT': 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
